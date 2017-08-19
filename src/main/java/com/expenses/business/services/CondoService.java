80325f6086794bc2bf5aaae36dd3864d978cc10c
valeriavamf <valeria,vamf@gmail.com>
2017-08-18 20:13:39 -0400

    add project

---
+++ src/main/java/com/expenses/business/services/CondoService.java
@@ -0,0 +1,8 @@
+package com.expenses.business.services;
+
+import com.expenses.business.model.Condo;
+
+public interface CondoService {
+
+    Condo registerCondo(Condo condo);
+}
---
+++ src/main/java/com/expenses/business/services/CondoServiceImpl.java
@@ -0,0 +1,12 @@
+package com.expenses.business.services;
+
+import com.expenses.business.model.Condo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CondoServiceImpl implements CondoService {
+
+    public Condo registerCondo(Condo condo) {
+        return null;
+    }
+}
